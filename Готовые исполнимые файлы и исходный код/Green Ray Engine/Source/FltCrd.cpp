c3a959ca5669fa91d52dee8d362e8e0e3bc39bf0
Artyom Kadirov <107424578+Kadirov-Artem@users.noreply.github.com>
2025-05-01 12:37:40 +0300

    Delete Готовые исполнимые файлы и исходный код directory

--- Готовые исполнимые файлы и исходный код/Green Ray Engine/Source/FltCrd.cpp
+++
@@ -1,6 +0,0 @@
-#include "FltCrd.h"
-
-bool gr::operator <(const FltCrd &l, const FltCrd &r)
-{
-	return l.y < r.y || l.y == r.y && l.x < r.x;
-}
--- Готовые исполнимые файлы и исходный код/Green Ray Engine/Source/IntCrd.cpp
+++
@@ -1,6 +0,0 @@
-#include "IntCrd.h"
-
-bool gr::operator <(const IntCrd &l, const IntCrd &r)
-{
-	return l.y < r.y || l.y == r.y && l.x < r.x;
-}
